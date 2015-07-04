66f5d5ca7b903ae5795234d37d579a75893489a9
Oliver Faircliff <mail@oliverfaircliff.com>
2015-07-04 23:42:18 +0100

    Correct logic

--- loader.c
+++ loader.c
@@ -9,7 +9,7 @@ texture load_b (char data[]) {
   texture tex = (texture)malloc(BLOCK_W * BLOCK_H * sizeof(color));
   color c;
 
-  for (int i = 0; i <= BLOCK_W * BLOCK_H; i++) {
+  for (int i = 0; i < BLOCK_W * BLOCK_H; i++) {
     c.r = (((data[0] - 33) << 2) | ((data[1] - 33) >> 4));
     c.g = ((((data[1] - 33) & 0xF) << 4) | ((data[2] - 33) >> 2));
     c.b = ((((data[2] - 33) & 0x3) << 6) | ((data[3] - 33)));
